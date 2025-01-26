856f2e2b5dbc6ba7f2b6d20af9f1e14a9d140f20
3psilon0 <bestofluck110@gmail.com>
2025-01-26 16:50:38 +0500

    Done

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-const library = [
-    {
-        title: 'The Way of Kings',
-        author: 'Brandon Sanderson',
-        genre: 'Fantasy',
-        pages: 1258,
-        read: true
-    }
-];
+const library = [];
 
 function Book(title, author, genre, pages, read = false) {
     this.title = title;
@@ -17,11 +9,11 @@ function Book(title, author, genre, pages, read = false) {
 }
 
 Book.prototype.toggleRead = function() {
-    this.read = !this.read;
+    this.read = !this.read; 
 }
 
 function addBookToLibrary(title, author, genre, pages, read = false) {
-    const book = new Book(title, author, genre, pages, read = false);
+    const book = new Book(title, author, genre, pages, read);
     library.push(book);
 }
 
@@ -33,12 +25,23 @@ function createBookCard(book) {
         <p>by</p>
         <h3 class="card-author">${book.author}</h3>
         <p>Genre: ${book.genre}</p>
-        <p>Pages: ${book.pages}</p>
+        <p>Pages: ${book.pages}</p> 
+        <p class="card-read-status">${book.read ? 'READ' : 'UNREAD'}</p>
         <div class="card-buttons">
-            <button class=${book.read ? 'card-read' : 'card-unread'}></button>
-            <button class="remove-book"></button>
+            <button id="toggle-button" class=${book.read ? 'card-unread' : 'card-read'}></button>
+            <button id="remove-button" class="remove-book" "></button>
         </div>
     `;
+    card.querySelector('#toggle-button').addEventListener('click', () => {
+        book.toggleRead();
+        renderLibrary();
+    });
+
+    card.querySelector('#remove-button').addEventListener('click', () => {
+        const index = library.indexOf(book);
+        library.splice(index, 1);
+        renderLibrary();
+    });
     return card;
 }
 
@@ -50,5 +53,34 @@ function renderLibrary() {
         container.appendChild(card);
     });
 }
+
+const addPopup = document.querySelector('.book-add-popup');
+const addButton = document.querySelector('.add-button');
+const confirmAddButton = document.querySelector('#confirm-add');
+const cancelAddButton = document.querySelector('#cancel-add');
+
+addButton.addEventListener('click', () => {
+    addPopup.showModal();
+});
+
+confirmAddButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    addBookToLibrary(
+        document.querySelector('#title').value,
+        document.querySelector('#author').value,
+        document.querySelector('#genre').value,
+        document.querySelector('#pages').value,
+        document.querySelector('#read').checked
+    );
+    renderLibrary();
+    addPopup.close();
+});
+
+cancelAddButton.addEventListener('click', () => {
+    addPopup.close();
+});
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 'Fantasy', 295, true);
+addBookToLibrary('The Great Gatsby', 'F. Scott Fitzgerald', 'Fiction', 180, false);
 
 renderLibrary();
